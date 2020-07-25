d33c7972ab0bfa53beb244486aaae4410a366baa
Morenikeji <bellokeji19@gmail.com>
2020-07-25 13:46:03 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('<h1>HELLO WORLD, WELCOME TO WEJAPA INTERNSHIPS</h1>'); //write a response
+  res.end(); //end the response
+}).listen(3000, function(){
+ console.log("listening on port 3000"); //the server object listens on port 3000
+});
